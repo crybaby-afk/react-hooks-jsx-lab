985d4cf0f58609e29a5f2e97282c2d0ebff58c27
crybaby-afk <toolarry55@gmail.com>
2024-12-03 11:41:21 +0300

    JSX

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "Larry";
+const city = "Eld0ret";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
